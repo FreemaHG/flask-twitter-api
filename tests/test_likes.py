3e3c4221182a3b85e9f7d4fdb34cac4c04a5e25f
“FreemaH” <stafeeff.dmitrij@yandex.ru>
2023-08-30 21:04:57 +0300

    Test. Question 4

--- tests/test_likes.py
+++ tests/test_likes.py
@@ -7,6 +7,7 @@ from app.models.users import User
 from app.models.tweets import Tweet, Like
 
 
+# 1 вариант (без твита)
 class TestApi:
 
     @pytest.fixture
@@ -30,6 +31,38 @@ class TestApi:
                     headers={"api-key": "test-user"},
                 )
             assert response
+            assert response.status_code == 200
+            assert response.json["result"]
+            assert delete_like_mock.called_once()
+
+
+# 2 вариант (с твитом)
+class TestApi2:
+
+    @pytest.fixture
+    # 1 - Создали экземпляр пользователя
+    def user_model(self):
+        return User(id=1, name="TestTest", api_key="test")
+
+    @patch("app.models.tweets.Like.delete")
+    def test_delete_like(self, delete_like_mock, client, user_model):
+            # 2 - создали экземпляр лайка, который будем удалять
+            like = Like(tweet_id=1, user_id=user_model.id)
+
+            with (
+                    # 3 - фиктивная проверка, что пользователь аутентифицирован (возвращаем mock-объект)
+                    patch("app.models.users.User.get_by_token", return_value=user_model),
+                    # 4 - фиктивная проверка, что лайк действительно есть (возвращаем mock-объект)
+                    patch("app.models.tweets.Like.get_by_ids", return_value=like),
+            ):
+                # TODO 5 - Выполняется реальный запрос по URL на удаление, во время которого выполняется запрос к БД
+                # TODO и т.к. в БД нет пользователя с api-key == test - тест проваливается из-за исключения 401 UNAUTHORIZED]
+                response = client.delete(
+                    "api/tweets/1/likes",
+                    headers={"api-key": "test"},
+                )
+            assert response
+            # TODO 6 - 401 != 200
             assert response.status_code == 200
             assert response.json["result"]
             assert delete_like_mock.called_once()
